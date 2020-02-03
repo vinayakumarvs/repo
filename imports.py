5fc7552e35aac208eaa3641a42afc87bc6eff82a
vinayakumarvs <vinayakumarvs@users.noreply.github.com>
2020-02-03 22:28:46 +0530

    Update imports.py

--- imports.py
+++ imports.py
@@ -8,4 +8,8 @@ import numpy as np
 import sys
 import math
 from google.colab import drive
+<<<<<<< HEAD
 from keras.datasets import cifar10, mnist
+=======
+from keras.datasets import cifar10, mnist
+>>>>>>> d423e0f8c75ea9193f7074cf151eb3282f6883e5
